74df8f838ec5f6d41a81a882bd63f96421114198
miraykksal <122979161+miraykksal@users.noreply.github.com>
2024-05-15 12:22:14 +0000

    footer güncellemesi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container, Button, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button} from 'react-bootstrap';
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -15,9 +15,9 @@ import PaymentPage from './pages/PaymentPage';
 import SelectSeatPage from './pages/SelectSeatPage';
 import AdminPanelPage from './pages/AdminPanelPage';
 import ChatSupport from './components/ChatSupport';
+import Footer from './components/Footer';
 
 import logoImage from './img/logo.png';
-import linkedinImage from './img/linkedin.png'; // LinkedIn resmi import edildi
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -34,7 +34,7 @@ const App = () => {
     <Router>
       <div className="App">
         <header>
-          <Navbar bg="light" expand="lg" className="nav-links">
+          <Navbar bg="light" expand="lg" className="navbar-links">
             <Container>
               <Navbar.Brand href="/">
                 <img src={logoImage} className="logo" alt="logo" />
@@ -42,13 +42,13 @@ const App = () => {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                  <Nav.Link as={NavLink} to="/" className="nav-link" activeClassName="active">Home</Nav.Link>
-                  <Nav.Link as={NavLink} to="/contact" className="nav-link" activeClassName="active">Contact</Nav.Link>
-                  <Nav.Link as={NavLink} to="/ticket-inquiry" className="nav-link" activeClassName="active">Ticket Inquiry</Nav.Link>
+                  <Nav.Link as={NavLink} to="/" className="navbar-link" activeClassName="active">Home</Nav.Link>
+                  <Nav.Link as={NavLink} to="/contact" className="navbar-link" activeClassName="active">Contact</Nav.Link>
+                  <Nav.Link as={NavLink} to="/ticket-inquiry" className="navbar-link" activeClassName="active">Ticket Inquiry</Nav.Link>
                   {isLoggedIn ? (
                     <Button onClick={handleLogout} className="login-button">Logout</Button>
                   ) : (
-                    <Nav.Link as={NavLink} to="/login" className="nav-link login-button" activeClassName="active">Login</Nav.Link>
+                    <Nav.Link as={NavLink} to="/login" className="navbar-link login-button" activeClassName="active">Login</Nav.Link>
                   )}
                 </Nav>
               </Navbar.Collapse>
@@ -71,81 +71,10 @@ const App = () => {
           </Routes>
           <ChatSupport />
         </main>
-        <footer className="bg-dark text-white text-center text-lg-start">
-          <Container className="p-4">
-            <Row>
-              <Col lg="4" md="12" className="mb-4 mb-md-0">
-                <h5 className="text-uppercase">About TrainTrack</h5>
-                <p>
-                  bla bla
-                </p>
-              </Col>
-              <Col lg="3" md="6" className="mb-4 mb-md-0">
-                <h5 className="text-uppercase">Quick Links</h5>
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <Nav.Link as={NavLink} to="/" className="text-white">Home</Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link as={NavLink} to="/contact" className="text-white">Contact</Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link as={NavLink} to="/ticket-inquiry" className="text-white">Ticket Inquiry</Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link as={NavLink} to="/login" className="text-white">Login</Nav.Link>
-                  </li>
-                </ul>
-              </Col>
-              <Col lg="3" md="6" className="mb-4 mb-md-0">
-                <h5 className="text-uppercase">Contact Us</h5>
-                <ul className="list-unstyled mb-0">
-                <li>
-                    <Button 
-                      variant="primary" 
-                      href="https://www.linkedin.com/in/yourprofile" 
-                      target="_blank" 
-                      rel="noopener noreferrer"
-                      className="linkedin-button"
-                    >
-                      <img src={linkedinImage} alt="LinkedIn" className="linkedin-icon" />
-                      Miray Köksal
-                    </Button>
-                  </li>
-                  <li>
-                    <Button 
-                      variant="primary" 
-                      href="https://www.linkedin.com/in/yourprofile" 
-                      target="_blank" 
-                      rel="noopener noreferrer"
-                      className="linkedin-button"
-                    >
-                      <img src={linkedinImage} alt="LinkedIn" className="linkedin-icon" />
-                      Cem Düşenkalkan
-                    </Button>
-                  </li>
-                  <li>
-                    <Button 
-                      variant="primary" 
-                      href="https://www.linkedin.com/in/yourprofile" 
-                      target="_blank" 
-                      rel="noopener noreferrer"
-                      className="linkedin-button"
-                    >
-                      <img src={linkedinImage} alt="LinkedIn" className="linkedin-icon" />
-                      Meryem Çanga
-                    </Button>
-                  </li>
-                  
-                </ul>
-              </Col>
-            </Row>
-          </Container>
-          <div className="text-center p-3 bg-secondary">
-            © 2024 TrainTrack. All rights reserved.
-          </div>
-        </footer>
       </div>
+
+      <Footer/>
+
     </Router>
   );
 };
