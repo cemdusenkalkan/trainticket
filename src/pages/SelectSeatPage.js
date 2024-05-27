25a6f8ead2c3dc80f3596c6ad2d79ffdee541c15
miraykksal <122979161+miraykksal@users.noreply.github.com>
2024-05-27 13:54:00 +0000

    select seat sayfası düzenlendi

--- src/pages/SelectSeatPage.js
+++ src/pages/SelectSeatPage.js
@@ -1,45 +1,58 @@
 import React, { useState } from 'react';
 import '../SelectSeat.css';
 
-const SelectSeatPage = () => {
-    const createSeats = () => {
-        let seats = [];
-        for (let row = 0; row < 4; row++) {
-          for (let num = 1; num <= 10; num++) {
-            seats.push({ id: `${row + 1}${String.fromCharCode(65 + num - 1)}`, occupied: false });
-          }
-        }
-        return seats;
-      };
 
-  const [seats, setSeats] = useState(createSeats());
-
-  const toggleSeatSelection = (id) => {
-    const newSeats = seats.map(seat => {
-      if (seat.id === id) {
-        return { ...seat, occupied: !seat.occupied };
-      }
-      return seat;
-    });
-    setSeats(newSeats);
+const SelectedSeatPage = () => {
+  const renderButtons = (start, count, char) => {
+    let buttons = [];
+    for (let i = start; i < count; i++) {
+      buttons.push(
+        <button key={i} className="m-2 seat">{i + 1}{char}</button>
+      );
+    }
+    return buttons;
   };
 
+
+
   return (
-    <div className="train-car">
-      <div className="train-container">
-        {seats.map((seat, index) => (
-          <button
-            key={seat.id}
-            className={`seat ${seat.occupied ? 'occupied' : ''}`}
-            onClick={() => toggleSeatSelection(seat.id)}
-            disabled={seat.occupied}
-          >
-            {seat.id}
-          </button>
-        ))}
+    <div className="bt-container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          {renderButtons(0, 6, 'A')}
+        </div>
+        <div className="col-auto">
+          {renderButtons(6, 12, 'A')}
+        </div>
+      </div>
+      <div className="row justify-content-center mb-5">
+        <div className="col-auto">
+          {renderButtons(0, 6, 'B')}
+        </div>
+        <div className="col-auto">
+          {renderButtons(6, 12, 'B')}
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          {renderButtons(0, 6, 'C')}
+        </div>
+        <div className="col-auto">
+          {renderButtons(6, 12, 'C')}
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          {renderButtons(0, 6, 'D')}
+        </div>
+        <div className="col-auto">
+          {renderButtons(6, 12, 'D')}
+        </div>
       </div>
     </div>
   );
 };
 
-export default SelectSeatPage;
+
+
+export default SelectedSeatPage;
